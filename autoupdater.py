bee3e0defdf6af0eb440fbc2df6afd0617a234a4
Arttu <huttunen.arttu@gmail.com>
2020-01-30 22:19:11 +0200

    Downloads new server jar if version written in local json differs from papermc api json

--- autoupdater.py
+++ autoupdater.py
@@ -3,11 +3,22 @@ import json
 import os
 
 
-os.chdir('~/survival')
+os.chdir('~/survival'')
 def responseList(version):
     url = 'https://papermc.io/api/v1/paper/' + version
     response = requests.get(url)
     return response.json()
+
+def download():
+    url = 'https://papermc.io/api/v1/paper/' + latestMCVersion + '/latest/download'
+
+    with open('paper-' + latestPaperVersion + '.jar', 'wb') as code:
+        code.write(requests.get(url).content)
+
+    versionData = {"MCVersion": latestMCVersion, "PaperVersion": latestPaperVersion}
+    with open('serverVersion.json', 'w') as jsonfile:
+        json.dump(versionData, jsonfile)
+
 
 latestMCVersion = responseList('')['versions'][0]
 print(latestMCVersion)
@@ -15,7 +26,17 @@ print(latestMCVersion)
 latestPaperVersion = responseList(latestMCVersion)['builds']['latest']
 print(latestPaperVersion)
 
-url = 'https://papermc.io/api/v1/paper/' + latestMCVersion + '/latest/download'
+currentMCVersion = ''
+currentPaperVersion = ''
+with open('serverVersion.json') as versionFile:
+    data = json.load(versionFile)
+    currentMCVersion = data['MCVersion']
+    currentPaperVersion = data['PaperVersion']
+    print(currentMCVersion)
+    print(currentPaperVersion)
 
-with open('paper-' + latestPaperVersion + '.jar', 'wb') as code:
-    code.write(requests.get(url).content)
+if (latestMCVersion != currentMCVersion):
+    download()
+elif (latestPaperVersion != currentPaperVersion):
+    download()
+
